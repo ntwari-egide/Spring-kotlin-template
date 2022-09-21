42ddffb0613d0e78745ab07bdbeca30bb8cb582c
ntwariegide2@ <ntwariegide2@gmail.com>
2022-09-21 19:13:48 +0200

    Ft: add library book model

---
+++ src/main/kotlin/com/student/management/domain/LibraryBook.kt
@@ -0,0 +1,19 @@
+package com.student.management.domain
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "library_books")
+class LibraryBook(
+        var name: String? = null,
+        var author: String? = null,
+        var price: Double? = null,
+        var quantity: Int? = null,
+        var status: String? = null
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    open var id: Long? = null
+
+}
