ac2e4db95008651c7f294a8236ec9e354e257e87
ntwariegide2@ <ntwariegide2@gmail.com>
2022-09-21 18:00:24 +0200

    Ft: add declaratives

--- src/main/kotlin/com/student/management/controller/StudentController.kt
+++ src/main/kotlin/com/student/management/controller/StudentController.kt
@@ -4,10 +4,15 @@ import com.student.management.domain.Student
 import com.student.management.dtos.StudentDto
 import com.student.management.serviceImpl.StudentServiceImpl
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController()
@@ -27,18 +32,35 @@ class StudentController (
     }
 
     @PostMapping
-    fun addStudent(student: StudentDto) : Student {
+    fun addStudent(@RequestBody student: StudentDto) : ResponseEntity<Any> {
 
-        return studentService.addStudent(student);
+        var savedStudent : Student = studentService.addStudent(student);
 
+        return ResponseEntity.ok(savedStudent)
     }
 
     @GetMapping("/byid/{id}")
-    fun getStudentById(id: Long) : Student {
+    fun getStudentById(@RequestParam id: Long) : Student {
 
         return studentService.getStudentById(id);
 
     }
+
+    @PutMapping("/update/{id}")
+    fun updateStudent(@RequestParam id: Long, @RequestParam student: StudentDto) : Student {
+
+        return studentService.updateStudent(id, student);
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    fun deleteStudent(id: Long) : Student {
+
+        return studentService.deleteStudent(id);
+
+    }
+
+
 
 
 }
--- src/main/kotlin/com/student/management/dtos/StudentDto.kt
+++ src/main/kotlin/com/student/management/dtos/StudentDto.kt
@@ -1,3 +1,3 @@
 package com.student.management.dtos
 
-data class StudentDto(val name: String, val age: Int, val address: String)
+data class StudentDto(val name: String = "student 1", val age: Int = 12, val address: String = "Rwanda")
