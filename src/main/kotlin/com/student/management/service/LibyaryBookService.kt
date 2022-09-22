a7a9724465beeab1d59feb1fb60afe16b6a5f288
ntwariegide2@ <ntwariegide2@gmail.com>
2022-09-22 22:09:58 +0200

    Ft: implement library book service implementation class

--- src/main/kotlin/com/student/management/service/LibyaryBookService.kt
+++ src/main/kotlin/com/student/management/service/LibyaryBookService.kt
@@ -5,16 +5,16 @@ import com.student.management.dtos.LibraryBookDto
 import org.springframework.stereotype.Service
 
 @Service
-interface LibyaryBookRepository  {
+interface LibyaryBookService  {
 
-    fun getBookById(id: Int): LibraryBook
+    fun getBookById(id: Long): LibraryBook
 
     fun getAllBooks(): List<LibraryBook>
 
     fun addBook(book: LibraryBookDto): LibraryBook
 
-    fun updateBook(book: LibraryBookDto): LibraryBook
+    fun updateBook(id: Long, book: LibraryBookDto): LibraryBook
 
-    fun deleteBook(id: Int): LibraryBook
+    fun deleteBook(id: Long): LibraryBook
 
 }
---
+++ src/main/kotlin/com/student/management/serviceImpl/LibryaryBookServiceImpl.kt
@@ -0,0 +1,65 @@
+package com.student.management.serviceImpl
+
+import com.student.management.domain.LibraryBook
+import com.student.management.dtos.LibraryBookDto
+import com.student.management.repository.LibraryBookRepository
+import com.student.management.service.LibyaryBookService
+import org.springframework.beans.factory.annotation.Autowired
+import java.util.Optional
+
+class LibryaryBookServiceImpl
+    (
+        @Autowired
+        private val libraryBookRepository: LibraryBookRepository
+    )
+    : LibyaryBookService {
+
+    fun checkExistence( id: Long ): LibraryBook {
+        var bookFound : Optional<LibraryBook> = libraryBookRepository.findById(id);
+
+        if(! bookFound.isPresent) throw Exception("Book not found with id: ${id}");
+
+        return bookFound.get();
+    }
+
+    override fun getBookById(id: Long): LibraryBook {
+        return checkExistence(id)
+    }
+
+    override fun getAllBooks(): List<LibraryBook> {
+        return libraryBookRepository.findAll();
+    }
+
+    override fun addBook(book: LibraryBookDto): LibraryBook {
+        var bookToSave = LibraryBook(
+            book.name,
+            book.author,
+            book.price,
+            book.quantity,
+            book.status
+        )
+
+        return libraryBookRepository.save(bookToSave);
+    }
+
+    override fun updateBook(id: Long, book: LibraryBookDto): LibraryBook {
+        var bookToUpdate = checkExistence(id);
+
+        bookToUpdate.name = book.name;
+        bookToUpdate.author = book.author;
+        bookToUpdate.price = book.price;
+        bookToUpdate.quantity = book.quantity;
+        bookToUpdate.status = book.status;
+        bookToUpdate.id = id
+
+        return libraryBookRepository.save(bookToUpdate);
+    }
+
+    override fun deleteBook(id: Long): LibraryBook {
+        var bookToDelete = checkExistence(id);
+
+        libraryBookRepository.delete(bookToDelete);
+
+        return bookToDelete;
+    }
+}
